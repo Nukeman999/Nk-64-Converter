9f4420130909928fed9a8b3d1e2793d31a747b67
Matt <39469614+KosmicShovel@users.noreply.github.com>
2020-09-29 01:09:49 -0500

    Initial Push

---
+++ NK64Converter/Compression.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.IO;
+
+namespace NK64Converter
+{
+    internal class Compression
+    {
+        public static bool Compress(string s)
+        {
+            using FileStream fs = File.Create("Output.NK64");
+            string? outputbase64 = Convert.ToBase64String(File.ReadAllBytes(s));
+            using StreamWriter sw = new StreamWriter(fs);
+            sw.Write(outputbase64);
+
+            FileInfo fi = new FileInfo(fs.Name);
+            return fi.Length != 0;
+        }
+
+        public static bool Decompress()
+        {
+            using FileStream fs = File.Create("New.swf");
+            byte[] bytes = Convert.FromBase64String(File.ReadAllText("Output.NK64"));
+            fs.Write(bytes, 0, bytes.Length);
+
+            FileInfo fi = new FileInfo(fs.Name);
+            return fi.Length != 0;
+        }
+    }
+}
---
+++ NK64Converter/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.IO;
+
+namespace NK64Converter
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            if (File.Exists("Input.swf"))
+            {
+                Console.WriteLine(Compression.Compress("Input.swf"));
+                Console.ReadKey();
+            }
+
+            Console.WriteLine(Compression.Decompress());
+            Console.ReadKey();
+        }
+    }
+}
