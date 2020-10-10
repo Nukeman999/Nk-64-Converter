7c95c46a4d39ab9a970de757cf152530a1df50bb
BowDown097 <btd6modsyeet@gmail.com>
2020-10-10 16:56:42 -0700

    Add Decrypt functionality

---
+++ NK64Converter/NKCrypt.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace NK64Converter
+{
+    class NKCrypt
+    {
+        private static byte[] Inflate(byte[] compressedBytes)
+        {
+            byte[] decompressedBytes = new byte[compressedBytes.Length];
+            using (MemoryStream ms = new MemoryStream(compressedBytes))
+            {
+                using (DeflateStream decompressionStream = new DeflateStream(ms, CompressionMode.Decompress))
+                {
+                    decompressionStream.Read(decompressedBytes, 0, compressedBytes.Length);
+                }
+            }
+
+            return decompressedBytes;
+        }
+
+        public static string Decrypt(byte[] ba)
+        {
+            int @char;
+            ba = Inflate(ba);
+            string @out = string.Empty;
+            string key = "vviiiojrnmasfncvviiihhbcjvhwopvviiioidychnxm";
+            int j = 0;
+
+            for (int i = 0; i < ba.Length; i++)
+            {
+                @char = ba[i] ^ key[j];
+                @out += (char)@char;
+                j++;
+                if (j >= key.Length)
+                {
+                    j = 0;
+                }
+            }
+
+            return @out;
+        }
+    }
+}
